f4be59715f17286eddebdce1fe210f54d7f25165
Kayaran <151110018+kayaran1999@users.noreply.github.com>
2024-01-11 10:24:25 +0530

    Update mario.c

--- mario/less/mario.c
+++ mario/less/mario.c
@@ -1,7 +1,28 @@
 #include <cs50.h>
 #include <stdio.h>
 
+#include <cs50>
+#include <stdio.h>
+
+int row, column, space;
+
+
 int main(void)
 {
+    int height;
+    do
+    {
+        height = get_int("Type the height here: ");
+    }
+    while (height < 1 || height > 8);
 
+    for(space= 0, space < height - row - column, space++)
+    for(row = 0, row < height, r++)
+    {
+        for(column = 0, column < row, column++)
+        {
+            printf("#");
+        }
+        printf("/n");
+    }
 }
